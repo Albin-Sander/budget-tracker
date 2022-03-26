ac61d1ab4207fea10af343d8a60baad7bcf32cfe
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2022-03-27 00:10:47 +0100

    deploy test again

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:albin-sander/albin-sander.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:albin-sander/budget-tracker.git main:gh-pages
